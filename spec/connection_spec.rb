9a80cfd37721f57ddd89267938a648c6dd2fe1e1
Trey Springer <trey@mustwin.com>
2014-03-19 13:34:24 -0500

    set subject of connection spec

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -1,40 +1,43 @@
 require 'spec_helper'
 
 describe Missinglink::Connection do
+  let(:subject) { Missinglink::Connection }
+
   context "credentialing" do
     before(:each) do
-      Missinglink::Connection.credential_hash = {}
+      subject.credential_hash = {}
     end
 
+
     it "should have a publicly accessible method to see if credentials were provided" do
-      Missinglink::Connection.credentials_provided?.should be_false
+      subject.credentials_provided?.should be_false
     end
 
     it "should not have an api key or token to start" do
-      Missinglink::Connection.credential_hash = {}
-      Missinglink::Connection.instance_variable_get("@credential_hash")[:api_key].should be_nil
-      Missinglink::Connection.instance_variable_get("@credential_hash")[:token].should be_nil
-      Missinglink::Connection.credentials_provided?.should be_false
+      subject.credential_hash = {}
+      subject.instance_variable_get("@credential_hash")[:api_key].should be_nil
+      subject.instance_variable_get("@credential_hash")[:token].should be_nil
+      subject.credentials_provided?.should be_false
     end
 
     it "should insist no credentials provided if only one of two pieces are present" do
-      Missinglink::Connection.credential_hash = { api_key: "key" }
-      Missinglink::Connection.credentials_provided?.should be_false
-      Missinglink::Connection.credential_hash = { token: "token" }
-      Missinglink::Connection.credentials_provided?.should be_false
+      subject.credential_hash = { api_key: "key" }
+      subject.credentials_provided?.should be_false
+      subject.credential_hash = { token: "token" }
+      subject.credentials_provided?.should be_false
     end
 
     it "should say it has its credentials if both api key and token are provided" do
-      Missinglink::Connection.credential_hash = ML_TEST_CREDS
-      Missinglink::Connection.credentials_provided?.should be_true
+      subject.credential_hash = ML_TEST_CREDS
+      subject.credentials_provided?.should be_true
     end
   end
 
   context "#request" do
     it "should not make a Typheous request unless there are credentials" do
-      Missinglink::Connection.stub(credentials_provided?: false)
+      subject.stub(credentials_provided?: false)
       Typhoeus::Request.should_not receive(:new)
-      Missinglink::Connection.request('get_survey_list')
+      subject.request('get_survey_list')
     end
   end
 end
