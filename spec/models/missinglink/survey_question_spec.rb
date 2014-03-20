3d6d6550782fac6f100944669769ae588bcedd3a
Trey Springer <trey@mustwin.com>
2014-03-20 18:40:21 -0400

    additional tests for survey question parsing

--- spec/models/missinglink/survey_question_spec.rb
+++ spec/models/missinglink/survey_question_spec.rb
@@ -3,6 +3,43 @@ require 'spec_helper'
 module Missinglink
   describe SurveyQuestion do
     let(:survey_question) { SurveyQuestion.new }
+    let(:survey_page) { SurveyPage.new }
+    let(:hash) { { "answers" => [],
+                   "type" => { "subtype" => "essay",
+                               "family" => "open_ended" },
+                   "position" => 4,
+                   "heading" => "Open Ended, Essay",
+                   "question_id" => "630844131" } }
+
+    context "#parse" do
+      before(:each) do
+        SurveyQuestion.stub(:first_or_create_by_sm_id => survey_question)
+      end
+
+      it "should grab the first question that matches the sm_id, or create it if need be" do
+        SurveyQuestion.should_receive(:first_or_create_by_sm_id).with("630844131")
+        SurveyQuestion.parse(survey_page, hash)
+      end
+
+      it "should update the question attributes as necessary" do
+        survey_question.should_receive(:update_attributes).with({ heading: "Open Ended, Essay",
+                                                                  position: 4,
+                                                                  type_family: "open_ended",
+                                                                  type_subtype: "essay"})
+        SurveyQuestion.parse(survey_page, hash)
+      end
+
+      it "sets the survey page for the question" do
+        survey_question.should_receive(:survey_pages=).with([survey_page])
+        SurveyQuestion.parse(survey_page, hash)
+      end
+
+      it "parses each answer" do
+        answer_hash = hash.merge("answers" => ["1","2"])
+        SurveyAnswer.should_receive(:parse).exactly(2).times
+        SurveyQuestion.parse(survey_page, answer_hash)
+      end
+    end
 
     context "#first_or_create_by_sm_id" do
       it "should create and return a new record if there is no question with the sm id" do
